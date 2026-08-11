7a7165472f08c2c61bcbf7fb6164f4d9185e0d1e
pparkcoder <pparkcoder@gmail.com>
2026-08-11 23:56:12 +0900

    refactor: ErrorCode 내 description 추가 (#2)

--- src/main/java/com/studyhub/member/domain/MemberStatus.java
+++ src/main/java/com/studyhub/member/domain/MemberStatus.java
@@ -1,5 +1,13 @@
 package com.studyhub.member.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberStatus {
-	ACTIVE, WITHDRAWN
+	ACTIVE("활성"),
+	WITHDRAWN("탈퇴");
+
+	private final String description;
 }
--- src/main/java/com/studyhub/member/domain/Role.java
+++ src/main/java/com/studyhub/member/domain/Role.java
@@ -1,5 +1,13 @@
 package com.studyhub.member.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-	MEMBER, OWNER
+	MEMBER("일반사용자"),
+	OWNER("업체관계자");
+
+	private final String description;
 }
