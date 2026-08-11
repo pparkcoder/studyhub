75334c846dcbd2d10d7386ff421dcdfb4ec2dce6
pparkcoder <pparkcoder@gmail.com>
2026-08-11 23:18:37 +0900

    refactor: DUPLICATE_USERNAME을 AuthErrorCode에서 MemberErrorCode로 이동 (#3)

--- src/main/java/com/studyhub/common/exception/AuthErrorCode.java
+++ src/main/java/com/studyhub/common/exception/AuthErrorCode.java
@@ -7,8 +7,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
 
-	DUPLICATE_USERNAME(HttpStatus.CONFLICT, "AUTH_001", "이미 사용 중인 아이디입니다."),
-	LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "AUTH_002", "아이디 또는 비밀번호가 일치하지 않습니다");
+	LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "AUTH_001", "아이디 또는 비밀번호가 일치하지 않습니다");
 
 	private final HttpStatus status;
 	private final String code;
