1ba808ec4d4e27b5461499aa6a3e8d8443c44439
pparkcoder <pparkcoder@gmail.com>
2026-08-11 23:17:13 +0900

    feat: member 엔티티 내 상태변경 메서드 추가 (#3)

--- src/main/java/com/studyhub/member/domain/Member.java
+++ src/main/java/com/studyhub/member/domain/Member.java
@@ -3,6 +3,7 @@ package com.studyhub.member.domain;
 import java.time.LocalDateTime;
 
 import com.studyhub.common.entity.BaseTimeEntity;
+import com.studyhub.member.dto.request.SignUpRequest;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -51,4 +53,42 @@ public class Member extends BaseTimeEntity {
 
 	@Column(name = "refresh_token_expired_at")
 	private LocalDateTime refreshTokenExpiredAt;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	private Member(String username, String nickname, String email, String password, Role role) {
+		this.username = username;
+		this.nickname = nickname;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.status = MemberStatus.ACTIVE;
+	}
+
+	public static Member from(SignUpRequest request, String password) {
+		return Member.builder()
+			.username(request.getUsername())
+			.nickname(request.getNickname())
+			.email(request.getEmail())
+			.password(password)
+			.role(request.getRole())
+			.build();
+	}
+
+	public void updateRefreshToken(String refreshTokenHash, LocalDateTime expiredAt) {
+		this.refreshTokenHash = refreshTokenHash;
+		this.refreshTokenExpiredAt = expiredAt;
+	}
+
+	public void clearRefreshToken() {
+		this.refreshTokenHash = null;
+		this.refreshTokenExpiredAt = null;
+	}
+
+	public void withdraw() {
+		this.status = MemberStatus.WITHDRAWN;
+	}
+
+	public void changePassword(String password) {
+		this.password = password;
+	}
 }
